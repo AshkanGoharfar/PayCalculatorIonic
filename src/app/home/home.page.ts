7a98703e828314b1bce8ecb1b836650c0dda9c17
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-02-08 13:39:30 -0500

    Main Screen has been implemented.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,28 @@ export class HomePage {
 
   constructor() {}
 
+  hourlyWorked;
+  result;
+  hourlyRate;
+  pay : number
+  overtimePay : number
+  totalPay : number
+  tax : number
+
+
+  calculatePay() {
+    if (this.hourlyWorked > 40)
+    {
+      this.pay  = 40 * this.hourlyRate
+      this.overtimePay = (this.hourlyWorked - 40) * this.hourlyRate *1.5 
+      this.totalPay = this.overtimePay+ this.pay  
+    }
+    else
+    {
+     this.pay = this.hourlyWorked * this.hourlyRate
+     this.totalPay = this.pay
+     this.overtimePay = 0.0
+    }
+    this.tax=  this.totalPay  * 0.18
+  }
 }
